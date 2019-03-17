b776c0a68d9d43221b82ce34a8475228401352af
Ардесов Вячеслав <me@arslav.ru>
2019-03-17 23:21:47 +0500

    Задание готово

--- Task 1/Form1.Designer.cs
+++ Task 1/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(654, 426);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Location = new System.Drawing.Point(673, 31);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(57, 17);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.Text = "Линия";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Location = new System.Drawing.Point(673, 54);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(105, 17);
+            this.radioButton2.TabIndex = 2;
+            this.radioButton2.Text = "Прямоугольник";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Location = new System.Drawing.Point(673, 77);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(87, 17);
+            this.radioButton3.TabIndex = 3;
+            this.radioButton3.Text = "Окружность";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(673, 415);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(115, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(673, 181);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(673, 210);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "label2";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- Task 1/Form1.cs
+++ Task 1/Form1.cs
@@ -12,9 +12,94 @@ namespace Task_1
 {
     public partial class Form1 : Form
     {
+        Point p1, p2;
+        bool _click = false;
+        Graphics g;
+        Pen pen = new Pen(Color.Black, 2);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.MouseClick += OnPictureBoxClicked;
+        }
+
+        private void OnPictureBoxClicked(object sender, MouseEventArgs e)
+        {
+            if (_click == false)
+            {
+                _click = true;
+                p1 = e.Location;
+                pictureBox1.MouseClick -= OnPictureBoxClicked;
+                label1.Text = p1.ToString();
+            }
+            else
+            {
+                _click = false;
+                p2 = e.Location;
+                pictureBox1.MouseClick -= OnPictureBoxClicked;
+                label2.Text = p2.ToString();
+                Drawing();
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pictureBox1.Image = null;
+        }
+
+        private void Drawing()
+        {
+            g = Graphics.FromHwnd(pictureBox1.Handle);
+            if(radioButton1.Checked)
+            {
+                g.DrawLine(pen, p1, p2);
+            }
+            else if(radioButton2.Checked)
+            {
+                g.DrawLine(pen, p1.X, p1.Y, p2.X, p1.Y);
+                g.DrawLine(pen, p1.X, p2.Y, p2.X, p2.Y);
+                g.DrawLine(pen, p1.X, p1.Y, p1.X, p2.Y);
+                g.DrawLine(pen, p2.X, p1.Y, p2.X, p2.Y);
+            }
+            else if(radioButton3.Checked)
+            {
+                int R = Math.Abs(p2.X - p1.X);
+                int X1 = p1.X;
+                int Y1 = p1.Y;
+                int x = 0;
+                int y = R;
+                int delta = 1 - 2 * R;
+                int error = 0;
+                while (y >= 0)
+                {
+                    drawpixel(g, X1 + x, Y1 + y);
+                    drawpixel(g, X1 + x, Y1 - y);
+                    drawpixel(g, X1 - x, Y1 + y);
+                    drawpixel(g, X1 - x, Y1 - y);
+                    error = 2 * (delta + y) - 1;
+                    if((delta < 0) && (error <= 0))
+                    {
+                        delta += 2 * ++x + 1;
+                        continue;
+                    }
+                    if ((delta > 0) && (error > 0))
+                    {
+                        delta -= 2 * --y + 1;
+                        continue;
+                    }
+                    delta += 2 * (++x - y--);
+                }
+            }
+        }
+
+        private void drawpixel(Graphics g, int v1, int v2)
+        {
+            g.FillRectangle(pen.Brush, v1, v2, 1, 1);
         }
     }
 }
